d51e5a5e0e18662b9b7c0ef99c8b4b55e7a24914
Skipsqueak514 <120784817+Skipsqueak514@users.noreply.github.com>
2023-03-19 13:38:15 +0600

    nujum

--- versions/constants.js
+++
@@ -1,9 +0,0 @@
-const constants = Object.freeze({
-    golangNode: "1.19.1",
-    golangApp: "1.19.1",
-    golangCore: "1.19.1",
-    golang: "1.19.1",
-    arabicaChainId: "arabica-6",
-  });
-  export default constants;
-  
